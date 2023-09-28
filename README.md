b96273752d00b4379c8f94da9138fdd6a6c4579b
Clayton Herbst <herbsca1@gmail.com>
2023-09-28 21:31:22 +1000

    chore(c++): setup examples folder

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Concurrency Primitives In Action
 
+## Getting Started
+
+### Initialize submodules
+
+```bash
+git submodule update --init --checkout
+```
---
+++ examples/c++/src/mutex.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex mtx; // Mutex to protect access to shared data
+int sharedData = 0; // Shared data
+
+const int numThreads = 5;
+
+void incrementData(int id) {
+    for (int i = 0; i < 5; ++i) {
+        // Lock the mutex to protect the critical section
+        std::unique_lock<std::mutex> lock(mtx);
+
+        // Simulate some work
+        std::this_thread::sleep_for(std::chrono::milliseconds(id));
+
+        // Increment the shared data
+        ++sharedData;
+        std::cout << "Thread " << id << " incremented sharedData to " << sharedData << std::endl;
+
+        // Unlock the mutex when done
+        lock.unlock();
+
+        // Continue with other work outside the critical section
+        std::this_thread::sleep_for(std::chrono::milliseconds(id));
+    }
+}
+
+int main() {
+    std::thread threads[numThreads];
+
+    // Create threads that increment the shared data
+    for (int i = 0; i < numThreads; ++i) {
+        threads[i] = std::thread(incrementData, i);
+    }
+
+    // Wait for all threads to finish
+    for (int i = 0; i < numThreads; ++i) {
+        threads[i].join();
+    }
+
+    // Print the final value of the shared data
+    std::cout << "Final sharedData value: " << sharedData << std::endl;
+
+    return 0;
+}
